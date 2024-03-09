e2a223f90ac35cbdb60350a6e2d8c76ca30615aa
Grigori Yudin <119896503+yudls@users.noreply.github.com>
2024-03-09 13:47:18 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,15 +16,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, BadLocationException {
-//        String path = "/Users/grisha/IdeaProjects/daiquiri/src/main/java/org/example/program.txt";
-//        String input = new String(Files.readAllBytes(Paths.get(path)), "UTF-8");
-//        final List<Token> tokens = new Lexer(input).tokenize();
-//            for (Token token : tokens)
-//                System.out.println(token.toString());
-//        final Statement program = new Parser(tokens).parse();
-//            System.out.println(program.toString());
-//        Interpreter interpreter = new Interpreter(program);
-//        interpreter.execute();
         Editor e = new Editor();
     }
 }
